315669f693bd29a281b4d2cc4e36fcd2bf8477ae
Renato Nunes Junior <renatonj0489@gmail.com>
2025-08-07 15:29:38 -0300

    Auto commit 2025-08-07_15-29

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,21 @@
 import "../styles/Container.css";
 import retangulos from "../assets/img/retangulos.png";
+import group1 from "../assets/img/group-1.png";
 
 export default function Container({ children }) {
   return (
     <main>
       <div className="container">
         {children}
-        <div className="retangulo">          
-                <img className="retangulos" src={retangulos} alt="Logo" />
+        <div className="retangulo">
+          <picture>
+            <source media="(min-width: 768px)" srcSet={group1} />
+            <img
+              className="retangulos"
+              src={retangulos}
+              alt="Imagem decorativa"
+            />
+          </picture>
         </div>
       </div>
       ;
