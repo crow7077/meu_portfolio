754f6f7075ae8920a4a70fe7896c8b5a04469a4e
Renato Nunes Junior <renatonj0489@gmail.com>
2025-08-20 17:59:38 -0300

    Auto commit 2025-08-20_17-59

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,6 +6,11 @@ import letraT from "../assets/img/letra-t-vetorizada.png";
 import folderCv from "../assets/img/folder.svg";
 import arrow from "../assets/img/arrow.svg";
 import tools from "../assets/img/tools.svg";
+import idenVisual from "../assets/img/identidade-visual.svg";
+import boxLogos from "../assets/img/boxLogos.svg";
+import phone from "../assets/img/phone.svg";
+import note from "../assets/img/notebook.svg";
+import paintBucket from "../assets/img/paint-bucket.svg";
 import aboutImg from "../assets/img/about-img.png";
 
 export default function Container({ children }) {
@@ -40,7 +45,7 @@ export default function Container({ children }) {
         {/* Seção Sobre Mim */}
 
         <div className="about">
-          <div className="about_title">
+          <div className="about-title">
             <h2>
               SOBRE
               <br />
@@ -97,10 +102,75 @@ export default function Container({ children }) {
         {/* Seção Bem Vindo */}
 
         <div className="bemVindo">
-          <h1>ASDV</h1>
+          <h3>
+            <p className="l1">BEM</p>
+            <p className="l2">VINDO</p>
+            <p className="l3">AO MEU</p>
+            <p className="l4">PORT</p>
+            <p className="l5">FÓLIO</p>
+            <p className="l6">DE</p>
+          </h3>
         </div>
-        
+
+        {/* Seção Bem Vindo */}
+
+        <div className="meusProjetos">
+          <img
+            className="idenVisual"
+            src={idenVisual}
+            alt=" identiade visual"
+          />
+          <div className="projetosWeb">
+            <div>
+              <h1>
+                Projeto
+                <br />
+                Web
+              </h1>
+            </div>
+            <img className="notebook" src={note} alt="Projeto Web" />
+          </div>
+          <div className="logos">
+            <div>
+              <h1>Logos</h1>
+              <img className="boxLogos" src={boxLogos} alt="Caixas de Logos" />
+            </div>
+          </div>
+          <div className="projetosMobile">
+            <div>
+              <h1>Mobile</h1>
+            </div>
+            <img className="phone" src={phone} alt="Celular" />
+          </div>
+        </div>
+
+        {/* Seção Bem Vindo */}
+
+        <div className="contacts">
+          <div className="contactDescription">
+            <h1>
+              <p>MEU ESTILO DE</p>
+              <br />
+              <p>
+                <span className="destaque">DESIGN</span> É UM
+              </p>
+              <br />
+              <p>REFLEXO DA MINHA</p>
+              <br />
+              <p>PERSONALIDADE.</p>
+            </h1>
+            
+        <img className="paint" src={paintBucket} alt="paint" />
+        <div className="box-yellow"></div>
+          </div>
+          <div className="entreEmContato">
+            <div>
+              <h1>ENTRE EM
+                <br />CONTATO</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
